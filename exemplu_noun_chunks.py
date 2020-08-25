7ccb4834e42953bda07975fc9204e94c17a7ad3f
Sergiu Nisioi <sergiu.nisioi@gmail.com>
2020-08-25 20:19:18 +0300

    todo in exemplu cu noun chunks

--- exemplu_noun_chunks.py
+++ exemplu_noun_chunks.py
@@ -7,4 +7,8 @@ txt = "Puttana — ma che cazzo ne sai tu ma la smetti"
 doc = nlp(txt)
 from noun_chunks import *
 ncs = noun_chunks(doc)
-chunks = [doc[nc[0]:nc[1]] for nc in ncs]
+chunks = [doc[nc[0]:nc[1]].text.lower() for nc in ncs]
+text_chunks = " ".join(chunks)
+#TODO:
+# 1 let's try tf-idf classification using text as noun chunks
+# 2 let's try text classification with noun chunks as they are
